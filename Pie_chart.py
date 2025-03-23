bc48c1029db032a45f8c7ed25c643430c8067ad5
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2025-03-23 17:42:44 +0100

    Update Pie_chart.py

--- Pie_chart.py
+++ Pie_chart.py
@@ -1,19 +1,13 @@
-#%%
+#%% Pie chart example
 import matplotlib.pyplot as plt
-
-
-#Pie chart example
-
 # Data
 labels = ['18-26 Jahre', '27-34 Jahre', '34-40 Jahre']
 sizes = [3, 27, 12]  # Absolute values (% needed)
 total = sum(sizes)
 percentages = [n / total * 100 for n in sizes] # to get percentages
 
-# Optional: colors
 colors = ['yellow', 'blue', 'green'] #['#ff9999','#66b3ff','#99ff99'] #either name them, or specify with #
 
-# Plot
 plt.figure(figsize=(8,10))
 plt.pie(percentages, labels=[f'{label} ({p:.1f}%)' for label, p in zip(labels, percentages)],
         autopct='%1.1f%%', startangle=140, colors=colors)
