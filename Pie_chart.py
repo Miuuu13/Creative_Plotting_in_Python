20ae4f667b9fdd13295d1ca78c8500e2ec5ba9c9
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2025-03-23 17:40:30 +0100

    Update Pie_chart.py

--- Pie_chart.py
+++ Pie_chart.py
@@ -11,10 +11,10 @@ total = sum(sizes)
 percentages = [n / total * 100 for n in sizes] # to get percentages
 
 # Optional: colors
-colors = ['yellow', 'blue', 'green'] ['#ff9999','#66b3ff','#99ff99']
+colors = ['yellow', 'blue', 'green'] #['#ff9999','#66b3ff','#99ff99'] #either name them, or specify with #
 
 # Plot
-plt.figure(figsize=(6,6))
+plt.figure(figsize=(8,10))
 plt.pie(percentages, labels=[f'{label} ({p:.1f}%)' for label, p in zip(labels, percentages)],
         autopct='%1.1f%%', startangle=140, colors=colors)
 plt.title('Altersgruppenverteilung (N=42)')
